851d8edc5f0b85aaa7ba45312f50a8a43a5ba969
gaoyulong <gaoyulong@xiaomi.com>
2023-02-22 17:14:26 +0800

    migrate to vela

--- runtime/ts_package.c
+++ runtime/ts_package.c
@@ -10,6 +10,7 @@
 
 #include <stdlib.h>
 
+#include "ts_built_in_modules.h"
 #include "ts_package.h"
 #include "ts_debug.h"
 
@@ -35,6 +36,65 @@ static void ts_remap_vtables(ts_vtable_pkg_t* mod_pkg_vt) {
   // remap others
 }
 
+/**
+ * @brief search the builtin module by url
+ * 
+ * @param path 
+ * @param szPath 
+ * @return ts_boolean_t 
+ */
+ts_boolean_t search_builtin_module(const char* path, char* szPath) {
+  for (int i = 0; built_in_modules_table[i].url; i++) {
+    if (strncmp(path, built_in_modules_table[i].url, 20) == 0) { // 内置模块名不能超过20个字符
+      strncpy(szPath, "built_in_", 9);
+      strncpy(szPath + 9, path, 20);
+      return 1;
+    }
+  }
+  return 0;
+}
+
+/**
+ * @brief return the builtin module info
+ * 
+ * @param path 
+ * @return ts_built_in_module_t* 
+ */
+ts_built_in_module_t* find_built_in_module(const char* path) {
+  ts_built_in_module_t *p = built_in_modules_table;
+  for (;p->module_name; p++) {
+    if (strncmp(p->module_name, path, 20) == 0) {
+      return p;
+    }
+  }
+  return NULL;
+}
+
+/**
+ * @brief load buildin modules into runtime
+ * 
+ * @param rt 
+ * @param path 
+ * @return ts_module_t* 
+ */
+static ts_module_t* ts_load_module_from_built_in(ts_runtime_t* rt, const char* path) {
+  ts_built_in_module_t* module_info = find_built_in_module(path);
+  if (!module_info) {
+    ts_error_log("cannot create module with entry from \"%s\"\n",
+		    path);
+    return NULL;
+  }
+  ts_module_t* m = module_info->module_register(rt);
+  if (m == NULL) {
+    ts_error_log("cannot create module with entry \"%s\" from \"%s\"\n",
+		    module_info->module_name, path);
+    return NULL;
+  }
+
+  m->package = module_info;
+
+  return m;
+}
 
 static ts_module_t* ts_load_module_from_package(ts_runtime_t* rt, const char* path) {
 
@@ -138,6 +198,9 @@ static ts_boolean_t find_module(const char* path, char* szPath, size_t size, con
 
 static ts_module_package_type_t ts_resolve_module_path(ts_runtime_t* rt, const char* path, char* szPath, size_t size, ts_module_package_type_t package) {
   szPath[0] = '\0';
+  if (search_builtin_module(path, szPath)) { //search built_in module, szPath得改一下名字
+    return ts_module_built_in;
+  }
 
   if (strchr(path, '/') != NULL) {  // is a path
     int fd = open(path, O_RDONLY);
@@ -172,11 +235,15 @@ static ts_module_package_type_t ts_resolve_module_path(ts_runtime_t* rt, const c
 TS_EXPORT ts_module_t* ts_load_module(ts_runtime_t* rt, const char* path, ts_module_package_type_t package) {
   char szPath[TS_STR_FORMAT_SIZE];
   package = ts_resolve_module_path(rt, path, szPath, sizeof(szPath), package);
-  
+
   if (package == ts_module_no_package) {
     ts_error_log("Unkwon package type of file \"%s\" (resolved path: \"%s\")",
                        path, szPath);
     return NULL;
+  }
+
+  if (package == ts_module_built_in) {
+    return ts_load_module_from_built_in(rt, szPath);
   }
 
   if (package == ts_module_package) {
