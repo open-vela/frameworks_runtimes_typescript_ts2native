d93991b8e3fdf39380a7b0b72f8b67b7b3a394a8
likeming <likeming@xiaomi.com>
2023-02-27 15:10:53 +0800

    feat: add date(tmp commit, DO NOT MERGE)
    Co-authored-by: huangyin5@xiaomi.com

---
+++ runtime/test/test_date.ts
@@ -0,0 +1,7 @@
+var date = new Date();
+console.log(date.getFullYear());
+date.setFullYear(2024);
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.toString());
+console.log(Date.now());
