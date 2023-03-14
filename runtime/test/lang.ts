278d95914a502b59e3277e3e0950a4c09b72dd75
dongjunjie <dongjunjie@xiaomi.com>
2023-03-14 21:40:13 +0800

    Remove the runtime from ts2native
    Change-Id: Ia2c08e5daf55ff9e960cf50e7e59b474e48dcd53

--- runtime/test/lang.ts
+++
@@ -1,7 +0,0 @@
-
-export type _int = number;
-export type _uint = number;
-export type _int64 = number;
-export type _uint64 = number;
-
-export type _struct<T> = T;
--- runtime/test/test_hello.ts
+++
@@ -1,6 +0,0 @@
-console.log("hello world!");
-console.info("info : hello world!");
-console.debug("debug : hello world!");
-console.warn("warn : hello world!");
-console.error("error : hello world!");
-console.trace();
