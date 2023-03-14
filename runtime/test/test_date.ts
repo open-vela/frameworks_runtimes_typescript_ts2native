07c10b3104681b21c639a95418aaa1386634ce94
dongjunjie <dongjunjie@xiaomi.com>
2023-03-14 21:40:13 +0800

    Remove the runtime from ts2native
    N/A
    
    Signed-off-by: dongjunjie <dongjunjie@xiaomi.com>

--- runtime/test/test_date.ts
+++
@@ -1,7 +0,0 @@
-var date = new Date();
-console.log(date.getFullYear());
-date.setFullYear(2024);
-console.log(date.getFullYear());
-console.log(date.getMonth());
-console.log(date.toString());
-console.log(Date.now());
